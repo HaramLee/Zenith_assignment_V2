261a9866d228a1af8f76d5e4188319404d5f0950
Haram Lee <haram.lee@hotmail.ca>
2016-11-23 16:53:39 -0800

    updated create event to have date picker added activity and event api page

---
+++ ZenithAssignment/src/ZenithAssignment/Controllers/ActivityApiController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZenithAssignment.Data;
+using ZenithAssignment.Models;
+
+namespace ZenithAssignment.Controllers
+{
+  
+    [Route("api/activity")]
+    public class ActivityApiController : Controller
+    {
+
+        private ApplicationDbContext _context { get; set; }
+
+        public ActivityApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Activity
+        [HttpGet]
+        public IEnumerable<Activity> Get()
+        {
+            return _context.Activities.ToList();
+        }
+
+        // GET api/Activity/5
+        [HttpGet("{id}")]
+        public Activity Get(int id)
+        {
+            return _context.Activities.FirstOrDefault(s => s.ActivityId == id);
+        }
+
+        // POST api/Activity
+        [HttpPost]
+        public void Post([FromBody]Activity activity)
+        {
+            _context.Activities.Add(activity);
+            _context.SaveChanges();
+        }
+
+        // PUT api/Activity/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Activity activity)
+        {
+            _context.Activities.Update(activity);
+            _context.SaveChanges();
+        }
+
+        // DELETE api/Activity/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var activity = _context.Activities.FirstOrDefault(t => t.ActivityId == id);
+            if (activity != null)
+            {
+                _context.Activities.Remove(activity);
+                _context.SaveChanges();
+            }
+        }
+    }
+
+
+}
---
+++ ZenithAssignment/src/ZenithAssignment/Controllers/EventApiController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZenithAssignment.Data;
+using ZenithAssignment.Models;
+
+namespace ZenithAssignment.Controllers
+{
+
+    [Route("api/event")]
+    public class EventApiController : Controller
+    {
+
+        private ApplicationDbContext _context { get; set; }
+
+        public EventApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Event
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return _context.Events.ToList();
+        }
+
+        // GET api/event/5
+        [HttpGet("{id}")]
+        public Event Get(int id)
+        {
+            return _context.Events.FirstOrDefault(s => s.EventId == id);
+        }
+
+        // POST api/event
+        [HttpPost]
+        public void Post([FromBody]Event events)
+        {
+            _context.Events.Add(events);
+            _context.SaveChanges();
+        }
+
+        // PUT api/event/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Event events)
+        {
+            _context.Events.Update(events);
+            _context.SaveChanges();
+        }
+
+        // DELETE api/event/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var events = _context.Events.FirstOrDefault(t => t.EventId == id);
+            if (events != null)
+            {
+                _context.Events.Remove(events);
+                _context.SaveChanges();
+            }
+        }
+
+
+    }
+}
