e883b72672303697b6256d90b7845f4859438740
Jordan Hamade <ardf2014@hotmail.com>
2016-11-24 23:06:30 -0800

    finished roles and did some clean up
    Signed-off-by: Jordan Hamade <ardf2014@hotmail.com>

--- ZenithAssignment/src/ZenithAssignment/Controllers/HomeController.cs
+++ ZenithAssignment/src/ZenithAssignment/Controllers/HomeController.cs
@@ -3,29 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ZenithAssignment.Data;
+using ZenithAssignment.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ZenithAssignment.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var today = DateTime.Now;
+            var events = db.Events.Include(a => a.Activity).Include(b => b.ApplicationUser);
+            var stuff = events.ToList();
+            var send = new List<Event>();
+            var datelist = new List<String>();
+            var someDay = today;
+            var week = new TimeSpan(6, 23, 59, 59);
+
+            if (today.DayOfWeek != DayOfWeek.Monday)
+            {
+                while (true)
+                {
+                    someDay = someDay.AddDays(-1);            //go backwards 1 day
+                    if (someDay.DayOfWeek == DayOfWeek.Monday)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var lastMonday = someDay.Date;
+            foreach (var x in stuff)
+            {
+
+                if (x.FromDate >= lastMonday && x.FromDate <= lastMonday + week && x.IsActive == true)
+                {
+
+                    if (!datelist.Contains(x.FromDate.ToString("dddd MMMM dd,yyyy")))
+                        datelist.Add(x.FromDate.ToString("dddd MMMM dd,yyyy"));
+
+                    send.Add(x);
+
+                }
+            }
+
+            List<Event> SortedList = send.OrderBy(o => o.FromDate).ToList();
+
+            var orderedList = datelist.OrderBy(x => DateTime.Parse(x)).ToList();
+
+            ViewData["MyData"] = orderedList;
+
+            return View(SortedList);
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
 
         public IActionResult Error()
         {
